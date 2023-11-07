72e252092e3dd7cf513a7a82a50d3325b9b5083f
19-BenjaminLee <leekyben@gmail.com>
2023-11-07 22:10:00 +0800

    Question 3a and 3b

--- Assets/02_MATRIX_worksheet/Matrix/HMatrix2D.cs
+++ Assets/02_MATRIX_worksheet/Matrix/HMatrix2D.cs
@@ -232,15 +232,24 @@ public class HMatrix2D : MonoBehaviour
         }
     }
 
-    //public void setTranslationMat(float transX, float transY)
-    //{
-    //    // your code here
-    //}
+    public void setTranslationMat(float transX, float transY)
+    {
+        // your code here
+        setIdentity();
+        entries[0, 2] = transX;
+        entries[0, 2] = transY;
+    }
 
-    //public void setRotationMat(float rotDeg)
-    //{
-    //    // your code here
-    //}
+    public void setRotationMat(float rotDeg)
+    {
+        // your code here
+        setIdentity();
+        float rad = rotDeg * Mathf.Deg2Rad;
+        entries[0, 0] = Mathf.Cos(rad);
+        entries[0, 1] = -Mathf.Sin(rad);
+        entries[1, 0] = Mathf.Sin(rad);
+        entries[1, 1] = Mathf.Cos(rad);
+    }
 
     //public void setScalingMat(float scaleX, float scaleY)
     //{
