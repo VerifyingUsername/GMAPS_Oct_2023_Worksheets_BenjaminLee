9409338efba3ff622a37310aa376c17088672227
19-BenjaminLee <leekyben@gmail.com>
2023-11-14 11:19:35 +0800

    Part 1 Question 3

--- Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/JumpToHeight/JumpToHeight.cs
@@ -1,37 +1,42 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class JumpToHeight : MonoBehaviour
-// {
-//     public float Height = 1f;
-//     Rigidbody rb;
+public class JumpToHeight : MonoBehaviour
+{
+    public float Height = 1f;
+    Rigidbody rb;
 
-//     private void Start()
-//     {
-//         rb = /*your code here*/;
-//     }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-//     void Jump()
-//     {
-//         // v*v = u*u + 2as
-//         // u*u = v*v - 2as
-//         // u = sqrt(v*v - 2as)
-//         // v = 0, u = ?, a = Physics.gravity, s = Height
+    void Jump()
+    {
+        // v*v = u*u + 2as
+        // u*u = v*v - 2as
+        // u = sqrt(v*v - 2as)
+        // v = 0, u = ?, a = Physics.gravity, s = Height
+        float v = 0f;
+        float a = Physics2D.gravity.y;
+        
 
-//         float u = Mathf.Sqrt(/*your code here*/);
-//         rb.velocity = new Vector3(/*your code here*/);
 
-//         //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
-//         //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-//     }
+        float u = Mathf.Sqrt(v * v - 2 * a * Height);
+        rb.velocity = new Vector3(0f, u, 0f);
 
-//     private void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.Space))
-//         {
-//             /*your code here*/();
-//         }
-//     }
-// }
+        //float jumpForce = Mathf.Sqrt(-2 * Physics2D.gravity.y * Height);
+        //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /*your code here*/
+            Jump();
+        }
+    }
+}
 
